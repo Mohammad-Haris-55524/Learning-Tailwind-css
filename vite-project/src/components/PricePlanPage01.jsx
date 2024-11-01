d42f6524424d406389f111a02153e8a1f188e218
Mohammad-Haris-55524 <mohammadharis10678@gmail.com>
2024-11-01 23:41:39 +0500

    Responsiveness added to Enterprise page

--- vite-project/src/components/PricePlanPage01.jsx
+++ vite-project/src/components/PricePlanPage01.jsx
@@ -3,41 +3,42 @@ import CardsOfCustomerStories from './CardsOfCustomerStories'
 
 function PricePlanPage01() {
 
-  const [moveBoxRight, setMoveBoxRight] = useState("")
-  const [moveBoxLeft, setMoveBoxLeft] = useState("")
+const [moveBoxLeftToRight, setMoveBoxLeftToRight] = useState(false)
+const [moveBoxRightToLeft, setmoveBoxRightToLeft] = useState(false)
 
-// const [toggleClass, setToggleClass] = useState("")
+console.log(moveBoxLeftToRight)
+console.log(moveBoxRightToLeft)
 
-  const handlerOne = () =>{
-console.log("1")
-setMoveBoxRight("class1")
-  }
-const handlerTwo = () =>{
-  console.log("2")
-setMoveBoxLeft("class2")
-}
-  
-  return (
+return (
   <section >
-{/* <div className='flex justify-between w-full'>
+<div className='flex justify-between w-full'>
   <div>
   <h1 className='font-bold text-5xl'>Choose a plan that fits your needs</h1> 
   </div>
   {/* px-4 py-4 hover:border-2 transition */} 
   {/* px-12 py-4 border-2 */}
   <div className='border-2 flex justify-between items-center rounded-lg w-[28%] px-12 py-4 '>
-    <div className={`border ${moveBoxRight} transition`}>
-    <button onClick={handlerOne} className='rounded-lg '><span>Monthly</span></button>
+    <div className='relative' >
+    <button className='z-50' onClick={()=>setMoveBoxLeftToRight(!moveBoxLeftToRight)}>Monthly
+    <div className={`border-4 border-secondary z-10 ${moveBoxLeftToRight ? "class1" : "class2"} transition rounded-lg absolute top-0
+    w-[10rem] h-[3rem]`}></div>
+    </button>
+
     </div>
-    <div className={`flex justify-center items-center gap-2 border ${moveBoxLeft} transition `}>
-    <button onClick={handlerTwo} className='rounded-lg '>Yearly
+    <div className={`flex justify-center items-center gap-2 border z-60 transition `}>
+    <div className='relative' >
+    <button className='z-50' onClick={()=>setmoveBoxRightToLeft(!moveBoxRightToLeft)}>Yearly
+    <div className={`border-4 border-secondary z-10 ${moveBoxRightToLeft ? "class2" : "class1"} transition rounded-lg absolute top-0
+    w-[10rem] h-[3rem]`}></div>
+
     </button>
     <span className='flex items-center justify-center w-12 h-7 p-1 rounded-full text-secondary text-sm font-bold border bg-[#d5ddff]  border-gray'>-20%</span>
+    </div>
 
     </div>
 </div>
 
-{/* </div> */} 
+</div>  
 
    <div className=' lg:flex justify-between '>
      
