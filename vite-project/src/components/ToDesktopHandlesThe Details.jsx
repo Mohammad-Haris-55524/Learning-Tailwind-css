72c10239ad022179ae04f43d036b4c94432db495
Mohammad-Haris-55524 <mohammadharis10678@gmail.com>
2024-11-10 23:59:35 +0500

    Added

---
+++ vite-project/src/components/ToDesktopHandlesThe Details.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function tToDesktopHandlesthe details() {
+  return (
+    <div>tToDesktop handles
+the details</div>
+  )
+}
+
+export default tToDesktop handles
+the details
